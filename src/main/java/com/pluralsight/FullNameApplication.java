8d2eec9b9b86d91c328acd3f1789d371715ed2f4
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-13 11:21:12 -0400

    completed first name scanner input

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello! Welcome to our full name generator!");
+        System.out.println("Please enter your details.");
+
+        System.out.print("First Name: ");
+        String firstName = input.next();
+
+        System.out.print("Middle Name: ");
+        System.out.print("Last Name: ");
+        System.out.println("Suffix: ");
+    }
 }
